5647d7d538c362780e3a8a2ec93b27b0bc70317c
Henrik <hflemmen@gmail.com>
2022-04-19 17:57:19 +0200

    Added the helper functions and helper classes

--- src/mrf/denoising.py
+++ src/mrf/denoising.py
@@ -1,24 +1,62 @@
 import MRF
+from typing import List
 
 
-class UnknownPixelNode(MRF.Node):
+class UnknownPixelNode(MRF.RandomNode):
     value: float
 
 
-class KnownPixelNode(MRF.Node):
+class KnownPixelNode(MRF.ObservedNode):
     value: float
 
 
 class LatentPixelFactor(MRF.Factor):
-    def evalueate(self, A: UnknownPixelNode, B: UnknownPixelNode):
-        return (A.value - B.value) ** 2
+
+    def __init__(self, gamma, beta):
+        self.gamma = gamma
+        self.beta = beta
+
+    def evalueate(self, xn: UnknownPixelNode, xm: UnknownPixelNode):
+        # TODO: Account for beta
+        return self.gamma * (xm.value - xn.value) ** 2
 
     def condition(self, A: UnknownPixelNode, B: UnknownPixelNode):
         pass
 
+
 class ImageConsistencyFactor(MRF.Factor):
-    def evalueate(self, A: UnknownPixelNode, B: KnownPixelNode):
-        return (A.value - B.value) ** 2
+
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def evalueate(self, xn: UnknownPixelNode, dn: KnownPixelNode):
+        return ((xn.value - dn.value) ** 2) / (2 * self.sigma ** 2)
 
     def condition(self, A: UnknownPixelNode, B: KnownPixelNode):
-        pass
+        if B is None:
+            raise ValueError("Can not condition on deterministic variable")
+        if A is None:
+            return
+
+
+def compute_from_neighbourhood(observed_pixel: KnownPixelNode,
+                               neighbouring_pixels: List[UnknownPixelNode],
+                               smoothing_factor: LatentPixelFactor,
+                               observed_image_factor: ImageConsistencyFactor):
+    """
+    This is created from the formulas in https://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/AV0809/ORCHARD/
+    :param observed_pixel:
+    :param neighbouring_pixels:
+    :param smoothing_factor:
+    :param observed_image_factor:
+    :return:
+    """
+    sum_of_neighbours = sum(map(lambda x: x.value, neighbouring_pixels))
+    # TODO Account for beta
+
+    return (2 * smoothing_factor.gamma * (
+            observed_image_factor.sigma ** 2) * sum_of_neighbours + observed_pixel.value) / (
+                   1 - 2 * smoothing_factor.gamma * len(neighbouring_pixels) * (observed_image_factor.sigma ** 2))
+
+
+
